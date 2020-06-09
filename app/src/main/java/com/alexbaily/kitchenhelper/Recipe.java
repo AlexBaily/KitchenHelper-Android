0fb9365eee1c38bf47598cc503c72ec52f70a6f4
AlexBaily <alex.d.baily@gmail.com>
2020-06-09 22:24:14 +0100

    Add tests and CardView

--- app/src/main/java/com/alexbaily/kitchenhelper/Recipe.java
+++ app/src/main/java/com/alexbaily/kitchenhelper/Recipe.java
@@ -7,13 +7,15 @@ public class Recipe {
 
     private int mRecipeId;
     private String mRecipeName;
+    private String mRecipeDescription;
     private ArrayList<Ingredient> mRecipeList;
     private ArrayList<RecipeStep> mRecipeSteps;
 
 
-    public Recipe(int mRecipeId, String mRecipeName) {
+    public Recipe(int mRecipeId, String mRecipeName, String mRecipeDescription) {
         this.mRecipeId = mRecipeId;
         this.mRecipeName = mRecipeName;
+        this.mRecipeDescription = mRecipeDescription;
         this.mRecipeList = new ArrayList<Ingredient>();
         this.mRecipeSteps = new ArrayList<RecipeStep>();
     }
@@ -34,4 +36,19 @@ public class Recipe {
         mRecipeSteps.remove(recipeStep);
     }
 
+    public String getmRecipeName() {
+        return mRecipeName;
+    }
+
+    public void setmRecipeName(String mRecipeName) {
+        this.mRecipeName = mRecipeName;
+    }
+
+    public String getmRecipeDescription() {
+        return mRecipeDescription;
+    }
+
+    public void setmRecipeDescription(String mRecipeDescription) {
+        this.mRecipeDescription = mRecipeDescription;
+    }
 }
