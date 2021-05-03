8f70e3391c97ad4522d2ad46a19a9fa8de2ee010
AlexBaily <alex.d.baily@gmail.com>
2021-05-03 10:52:11 +0100

    Add start of Auth0

---
+++ app/src/main/java/com/alexbaily/kitchenhelper/auth0.java
@@ -0,0 +1,10 @@
+package com.alexbaily.kitchenhelper;
+import com.auth0.android.Auth0;
+import com.auth0.android.provider.WebAuthProvider;
+
+public class auth0 {
+
+    private Auth0 account;
+
+    
+}
